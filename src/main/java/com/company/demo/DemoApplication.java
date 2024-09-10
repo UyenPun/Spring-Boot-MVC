3f259c660adb1c7de5d90a9564ed48d40407f237
UyenPun <tranuyen2018@gmail.com>
2024-09-10 18:35:04 +0700

    fix mysql và version dependencies.

--- src/main/java/com/company/demo/DemoApplication.java
+++ src/main/java/com/company/demo/DemoApplication.java
@@ -3,7 +3,6 @@ package com.company.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class DemoApplication {
 
