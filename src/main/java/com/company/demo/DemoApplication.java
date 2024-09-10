d37d1845c18ac66478ba1dfa446ba88064be8707
UyenPun <tranuyen2018@gmail.com>
2024-09-10 20:50:22 +0700

    Disable Security.

--- src/main/java/com/company/demo/DemoApplication.java
+++ src/main/java/com/company/demo/DemoApplication.java
@@ -3,7 +3,10 @@ package com.company.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+// @SpringBootApplication
+// include (bao gồm) >< exclude (loại bỏ)
+// Disable Security: để không cần nhập username/password
+@SpringBootApplication(exclude = org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)
 public class DemoApplication {
 
 	public static void main(String[] args) {
