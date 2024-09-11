96fe09c43c1bed7e442082d5ad8d780309b5aefa
UyenPun <tranuyen2018@gmail.com>
2024-09-11 12:24:52 +0700

    46 Render view HTML.

--- src/main/java/com/company/controller/UserController.java
+++ src/main/java/com/company/controller/UserController.java
@@ -2,29 +2,34 @@ package com.company.controller;
 
 import com.company.service.UserService;
 
-// import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController // Tự động trả về chuỗi hoặc dữ liệu thay vì view
+@Controller
 public class UserController {
-  // @RequestMapping("/")
-  // public String getHomePage() {
-  // return "Hello Home Page";
-  // }
 
-  // DI: dependency injection
-  private UserService userService;
-
-  public UserController(UserService userService) {
-    this.userService = userService;
-  }
-
-  @GetMapping("")
+  @RequestMapping("/")
   public String getHomePage() {
-    return this.userService.handleHello();
+    return "index.html"; // http://localhost:8080/index.html
   }
 
 }
+// ----------------------------------------------------------------
+// @RestController // Tự động trả về chuỗi hoặc dữ liệu thay vì view
+// public class UserController {
+// // DI: dependency injection
+// private UserService userService;
+
+// public UserController(UserService userService) {
+// this.userService = userService;
+// }
+
+// @GetMapping("")
+// public String getHomePage() {
+// return this.userService.handleHello();
+// }
+
+// }
